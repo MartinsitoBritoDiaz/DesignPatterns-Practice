59756c3c2832af99f4ecde075ffa404d78932a17
MartinsitoBritoDiaz <martinsitobd@gmail.com>
2023-06-08 22:52:09 -0400

    feat: asp dot net lock and sealed implementation

--- Tools/Log.cs
+++ Tools/Log.cs
@@ -1,13 +1,21 @@
 ﻿namespace Tools
 {
+
+    //Sealed (Sellado) to protect our class of being inherited
     public sealed class Log
     {
         private static Log _instance = null;
         private string _path;
+        private static readonly object _protect = new object();
 
         public static Log GetInstance(string path)
         {
-            _instance ??= new Log(path);
+            //lock is useful when we want to protected one peace of code of execute in various threads at the same time
+            //,so we use lock to protect, so only one thread can go inside at the same time
+            lock (_protect)
+            {
+                _instance ??= new Log(path);
+            }
 
             return _instance;
         }
