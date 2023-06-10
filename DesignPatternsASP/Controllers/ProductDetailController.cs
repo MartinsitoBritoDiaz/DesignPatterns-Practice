e19479b853f33f6ce72bf58488928ac9f0041a34
MartinsitoBritoDiaz <martinsitobd@gmail.com>
2023-06-09 23:27:10 -0400

    feat: factory design pattern asp

---
+++ DesignPatternsASP/Controllers/ProductDetailController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tools.Earn;
+
+namespace DesignPatternsASP.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        public IActionResult Index(decimal total)
+        {
+            //Factories
+            LocalEarnFactory localEarnFactory = new LocalEarnFactory(0.15m);
+            ForeignEarnFactory foreignEarnFactory = new ForeignEarnFactory(0.35m, 50);
+
+            //Products
+            IEarn localEarn = localEarnFactory.GetEarn();
+            IEarn foreignEarn = foreignEarnFactory.GetEarn();
+
+            ViewBag.totalLocal = total + localEarn.Earn(350);
+            ViewBag.totalForeign = total + foreignEarn.Earn(350);
+            return View();
+        }
+    }
+}
