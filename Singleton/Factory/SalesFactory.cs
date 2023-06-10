519d06bee4e94d4cc1e8bd327cad91d4fc184628
MartinsitoBritoDiaz <martinsitobd@gmail.com>
2023-06-09 22:16:51 -0400

    feat: factory design pattern cl

---
+++ Singleton/Factory/SalesFactory.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Factory
+{
+    //Creator
+    public abstract class SalesFactory
+    {
+        public abstract ISale GetSale();
+    }
+
+    //Concrete Creator
+    public class StoreSaleFactory : SalesFactory
+    {
+        private decimal _extra;
+        public StoreSaleFactory(decimal extra)
+        {
+            _extra = extra;
+        }
+
+        public override ISale GetSale()
+        {
+            return new StoreSale(_extra);
+        }
+    }
+
+    //Concrete Product
+    public class StoreSale : ISale
+    {
+        private readonly decimal _extra;
+        public StoreSale(decimal extra)
+        {
+            _extra = extra;
+        }
+        public void Sale(decimal amount)
+        {
+            Console.WriteLine($"Store sale has an amount of {amount + _extra}");
+        }
+    }
+
+    //Product
+    public interface ISale
+    {
+        public void Sale(decimal amount);
+    }
+
+    //Concrete Creator Internet
+    public class InternetSaleFactory : SalesFactory
+    {
+        private decimal _discount;
+        public InternetSaleFactory(decimal discount)
+        {
+            _discount = discount;
+        }
+
+        public override ISale GetSale()
+        {
+            return new InternetSale(_discount);
+        }
+    }
+
+    //Concrete Product Internet
+    public class InternetSale : ISale
+    {
+        private readonly decimal _discount;
+        public InternetSale(decimal discount)
+        {
+            _discount = discount;
+        }
+        public void Sale(decimal amount)
+        {
+            Console.WriteLine($"Internet sale has an amount of {amount - _discount}");
+        }
+    }
+
+
+}
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,7 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using Singleton.Models;
+// SINGLETON
+//using Singleton.Models;
 
-var example = Log.Instance;
-example.Save("This is a test");
-example.Save("Hello world");
+//var example = Log.Instance;
+//example.Save("This is a test");
+//example.Save("Hello world");
+
+
+// Factory
+using DesignPatterns.Factory;
+
+SalesFactory storeSaleDactory = new StoreSaleFactory(20);
+SalesFactory internetSaleDactory = new InternetSaleFactory(20);
+
+ISale storeSale = storeSaleDactory.GetSale();
+storeSale.Sale(1000);
+
+ISale internetSale = internetSaleDactory.GetSale();
+internetSale.Sale(1000);
